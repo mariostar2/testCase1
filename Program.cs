d685a6bf3015cff53f883131cbf232d3d5dbdfb3
KIM <alskfl7777@naver.com>
2023-02-20 13:45:58 +0900

    테스트 파일

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+enum WEAPON
+{
+    Melee,          //근접 공격
+    Long,           //원거리 공격
+    Throw,
+}
+
+
+class Weapon
+{
+
+}
+namespace Program_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WEAPON weapon = 0;
+            bool isMelee = true;
+            while (true)
+            {
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if(keyInfo.Key == ConsoleKey.F1)
+                {
+                    weapon = WEAPON.Melee;
+                }
+            }
+            
+        }
+    }
+}
